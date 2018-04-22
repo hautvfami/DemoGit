b9cf4c868559516d4b79a11d054365d7134bb21e
hautvfami <hautv.fami@gmail.com>
2018-04-22 09:59:15 +0700

    update tinh nang search

---
+++ app/src/main/java/dev/japango/demogit/MainActivity.java
@@ -0,0 +1,14 @@
+package dev.japango.demogit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //fsefsef
+    }
+}
